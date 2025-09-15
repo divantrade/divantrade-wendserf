347878d7d44988bc5e1a4e7fba4b4f1940541fe8
Adelsolimaan <info@divantrade.net>
2025-09-15 21:16:23 +0300

    chore(i18n): add next-intl.config.mjs (runtime config)

--- next-intl.config.ts
+++
@@ -1,9 +0,0 @@
-export const locales = ['en', 'ar'] as const;
-export const defaultLocale = 'en';
-export const localePrefix = 'always' as const;
-
-export default {
-  locales,
-  defaultLocale,
-  localePrefix
-};
